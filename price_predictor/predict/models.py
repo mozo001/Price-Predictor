943f6a6307eb0db5d45f37995d4034c92b829413
Mafiz Uddin Chowdhury <mafizchowdhury006@gmail.com>
2026-04-28 21:59:00 +0600

    Delete price_predictor directory

--- price_predictor/predict/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Prediction(models.Model):
-    feature1 = models.FloatField()
-    feature2 = models.FloatField()
-    predicted_price = models.FloatField()
