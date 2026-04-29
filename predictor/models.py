37fdffabefd722bf6ae2d0c261537bd8b951cb43
mafiz <mafizchowdhury006@gmail.com>
2026-04-29 20:04:05 +0600

    all for today

--- predictor/models.py
+++ predictor/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Laptop(models.Model):
+    # 'null=True, blank=True' allows the field to be empty (triggering our imputer!)
+    condition = models.FloatField(null=True, blank=True) 
+    age = models.FloatField(null=True, blank=True)
+    
+    # We will save the prediction here after the ML model runs
+    predicted_price = models.FloatField(null=True, blank=True)
+    
+    # Keeps track of when the prediction was made
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Laptop - Prediction: {self.predicted_price}"
--- predictor/views.py
+++ predictor/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Laptop
 # Create your views here.
+def index(request):
+    laptops = Laptop.objects.all()
+    return render(request, 'predictor/index.html', {'laptops': laptops})
