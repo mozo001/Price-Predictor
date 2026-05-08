8f0cd94edd6ad9cdccd9ef925df897061f0bffdd
mafiz <mafizchowdhury006@gmail.com>
2026-05-08 19:25:17 +0600

    fix SECRET_KEY issue

--- smart_inventory/settings.py
+++ smart_inventory/settings.py
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3t_srqdh+$y19!8slb5v(&(^qn8$j@crcl6)r7sia_xk$f%=xk'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['price-predictor-tkmk.onrender.com', 'localhost', '127.0.0.1']
 
